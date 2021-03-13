11a84efcd96d20954e1c01c5d92dd3aeb2c4633b
Daniil Boussel <daniil7142@gmail.com>
2021-03-12 22:38:17 -0500

    feat: fix test

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 module.exports = function toReadable(number) {
-    let numArr = [number].join().split('')
+
+    let numArr = [number].join().split('');
     let h;
     let t;
     let o;
     if (number >= 100) {
-        h = numArr[0]
-        t = numArr[1]
-        o = numArr[2]
+        h = numArr[0];
+        t = numArr[1];
+        o = numArr[2];
+
+        if (t === '0' && o === '0') {
+            t = !t
+            o = !o
+        };
         if (t === '1' || o === '0') {
             o = [t, o].join('')
             t = !t
-        }
+        };
         if (t === '0') {
             o = numArr[2]
             t = !t
-        }
+        };
     };
     if (number < 100) {
-        numArr.length > 1 ? (t = numArr[0], o = numArr[1]) : o = numArr[0]
-
-        if (t === '1') {
+        numArr.length > 1 ? (t = numArr[0], o = numArr[1]) : o = numArr[0];
+        if (numArr.length === 1 && o === '0') {
+            o = numArr[0]
+        } else if (t === '1' || o === '0') {
             o = [t, o].join('')
             t = !t
-        }
+        };
     };
     const nums = {
         1: 'one',
@@ -45,18 +52,21 @@ module.exports = function toReadable(number) {
         18: 'eighteen',
         19: 'nineteen',
         0: 'zero',
-        20: 'twenty ',
-        30: 'thirty ',
-        40: 'forty ',
-        50: 'fifty ',
-        60: 'sixty ',
-        70: 'seventy ',
-        80: 'eighty ',
-        90: 'ninety ',
-        100: 'hundred ',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred',
     };
     if (number >= 100) {
-        return t ? `${nums[h]} hundred ${nums[t*10]}${nums[o]}` : `${nums[h]} hundred ${nums[o]}`
+        if (!t && !o) {
+            return `${nums[h]} hundred`
+        }
+        return t ? `${nums[h]} hundred ${nums[t*10]} ${nums[o]}` : `${nums[h]} hundred ${nums[o]}`
     }
-    return t ? nums[t * 10] + nums[o] : nums[o]
+    return t ? nums[t * 10] + ' ' + nums[o] : nums[o]
 }
